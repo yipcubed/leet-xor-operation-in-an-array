001251220af9c509b60ad599b6c6f0476d4f6cc6
Yusuf Pisan <pisan@uw.edu>
2020-10-08 14:11:21 -0700

    start

---
+++ main.cpp
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include <climits>
+#include <cstring>
+#include <functional>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "../utils/PrintUtils.hpp"
+#include "../utils/VectorUtils.hpp"
+// #include "../utils/HeapUtils.hpp"
+// #include "../utils/BinarySearch.hpp"
+// #include "../utils/TreeUtils.hpp"
+
+using namespace std;
+
+#pragma GCC diagnostic ignored "-Wunknown-pragmas"
+
+// https://leetcode.com/problems/check-if-it-is-a-straight-line/
+// Live coding problems, watch at
+// https://www.twitch.tv/yipcubed
+// https://www.youtube.com/channel/UCTV_UOPu7EWXvYWsBFxMsSA/videos
+//
+
+// makes code faster, but larger. Just for LeetCode fun!
+#pragma GCC optimise("Ofast")
+
+// makes stdin not synced so it is faster. Just for LeetCode fun!
+static auto __ __attribute__((unused)) = []() { // NOLINT
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  return nullptr;
+}();
+
+class Solution {
+public:
+  bool func(vector<int> &arr) { return true; }
+};
+
+void test1() {
+  cout << boolalpha;
+  vector<int> arr{1, 2, 3};
+
+  cout << "1 ? " << Solution().func(arr) << endl;
+}
+
+void test2() {}
+
+void test3() {}
